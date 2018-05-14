3c6451346403d62ad1b2fdb446afd18a96bd4dc4
Zachary Jones <zrjones12@gmail.com>
2018-05-14 14:02:18 -0600

    Merged Rev 13 Received source for Rev 13 from true master.

--- ebook.js
+++ ebook.js
@@ -1,8 +1,8 @@
-var cheerio = require('cheerio');
-var fs = require('fs');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
-var ERROR_TAG = '[\033[91mError\033[0m]: ';
-var DEBUG = false;
+const ERROR_TAG = '[\033[91mError\033[0m]: ';
+const DEBUG = false;
 
 if(process.argv.length < 3)
 {
@@ -12,10 +12,10 @@ if(process.argv.length < 3)
 
 function ensure_dir(dir)
 {
-    dir = __dirname + '/' + dir;
-
-    if(!fs.existsSync(dir))
-	fs.mkdirSync(dir);
+	const full_path = __dirname + '/' + dir;
+	
+	if(!fs.existsSync(full_path))
+		fs.mkdirSync(full_path);
 }
 
 // Ensure the 'cache' and 'output' directories exists. Create them if they do not.
@@ -24,25 +24,25 @@ ensure_dir('output');
 
 function decode_cr(cr)
 {
-    var ishex = cr[2] === 'x';
-
-    return String.fromCodePoint(parseInt(cr.substr(ishex ? 3 : 2, cr.length - 2), ishex ? 16 : 10));
+	const ishex = cr[2] === 'x';
+	
+	return String.fromCodePoint(parseInt(cr.substr(ishex ? 3 : 2, cr.length - 2), ishex ? 16 : 10));
 }
 
 // Decode all HTML character references to unicode.
 function decode_crs(s)
 {
-    var i = -1;
-    var ls = s;
-
-    while((i = ls.search(/&#.*;/)) > -1)
-    {
-	var ni = ls.indexOf(';', i);
-
-	ls = ls.substr(0, i) + decode_cr(ls.substr(i, ni - i + 1)) + ls.substr(ni + 1);
-    }
-
-    return ls;
+	let i = -1;
+	let ls = s;
+	
+	while((i = ls.search(/&#.*;/)) > -1)
+	{
+		const ni = ls.indexOf(';', i);
+		
+		ls = ls.substr(0, i) + decode_cr(ls.substr(i, ni - i + 1)) + ls.substr(ni + 1);
+	}
+	
+	return ls;
 }
 
 function unescape_html(html)
@@ -58,37 +58,37 @@ function unescape_html(html)
 
 function purge(set)
 {
-    for(var i = 0; i < set.length; i++)
-    {
-	var e = set[i];
-
-	if(DEBUG)
-	    console.log('[\033[91mDELETE\033[0m]: [' + e.text() + ']');
-
-	e.remove();
-    }
+	for(let i = 0; i < set.length; i++)
+	{
+		var e = set[i];
+		
+		if(DEBUG)
+			console.log('[\033[91mDELETE\033[0m]: [' + e.text() + ']');
+			
+		e.remove();
+	}
 }
 
 function UriCache()
 {
     this.cache = [];
 
-    var files = fs.readdirSync(__dirname + '/cache');
+    const files = fs.readdirSync(__dirname + '/cache');
 
-    for(var i = 0; i < files.length; i++)
-	this.cache.push(files[i]);
+    for(let i = 0; i < files.length; i++)
+        this.cache.push(files[i]);
 }
 
 function FilterManager()
 {
     this.filters = {};
 
-    var files = fs.readdirSync(__dirname + '/filters');
+    const files = fs.readdirSync(__dirname + '/filters');
 
-    for(var i = 0; i < files.length; i++)
+    for(let i = 0; i < files.length; i++)
     {
-	var fname = files[i];
-	var fid = fname.substr(0, fname.length - 3);
+        const fname = files[i];
+        const fid = fname.substr(0, fname.length - 3);
 
 	this.filters[fid] = require('./filters/' + fid);
     }
@@ -96,7 +96,7 @@ function FilterManager()
 
 FilterManager.prototype.get = function(fid)
 {
-    var filter = this.filters[fid];
+    const filter = this.filters[fid];
 
     if(!filter)
     {
@@ -107,24 +107,24 @@ FilterManager.prototype.get = function(fid)
     return filter.apply;
 };
 
-var filter_mgr = new FilterManager();
+const filter_mgr = new FilterManager();
 
 function Finalize(params)
 {
-    var spec = params.spec;
+    const spec = params.spec;
 
     if(++spec.loaded === spec.contents.length)
     {
 	params.chap = null;
 
-	if(spec.output.constructor === String)
-	    filter_mgr.get(spec.output)(params, function(){});
-	else if(spec.output instanceof Array)
-	{
-	    var ops = [];
+        if(spec.output.constructor === String)
+            filter_mgr.get(spec.output)(params, function(){});
+        else if(spec.output instanceof Array)
+        {
+            let ops = [];
 
-	    for(var i = 0; i < spec.output.length; i++)
-		ops.push(filter_mgr.get(spec.output[i]));
+            for(let i = 0; i < spec.output.length; i++)
+                ops.push(filter_mgr.get(spec.output[i]));
 
 	    Sequence(ops, params);
 	}
@@ -138,7 +138,7 @@ function Sequence(ops, params, cb)
     if(ops.length < 2)
 	throw new Exception(ERROR_TAG + 'Cannot create a sequence of less than two operations.');
 
-    var last = function(params, cb) { return function() { Finalize(params); if(cb) cb(); }; }(params, cb);
+    let last = function(params, cb) { return function() { Finalize(params); if(cb) cb(); }; }(params, cb);
 
     for(var i = ops.length - 1; i >= 0; i--)
 	last = function(cur, nxt) { return function() { cur(params, nxt); }; }(ops[i], last);
@@ -147,14 +147,15 @@ function Sequence(ops, params, cb)
 }
 
 // Load the spec. Start processing.
-var spec = JSON.parse(fs.readFileSync(__dirname + '/' + process.argv[2]));
-var sched = {};
-var uri_cache = new UriCache();
+const spec = JSON.parse(fs.readFileSync(__dirname + '/' + process.argv[2]));
+const sched = {};
+const uri_cache = new UriCache();
 
 spec.loaded = 0;
 
-for(var i = 0; i < spec.contents.length; i++)
+for(let i = 0; i < spec.contents.length; i++)
 {
+<<<<<<< HEAD
     var chap = spec.contents[i];
     var params = {
 	spec: spec,
@@ -164,6 +165,17 @@ for(var i = 0; i < spec.contents.length; i++)
 	purge: purge,
 	uri_cache: uri_cache,
 	cheerio_flags: { decodeEntities: false }
+=======
+	const chap = spec.contents[i];
+    const params = {
+        spec: spec,
+        chap: chap,
+    	unescape_html: unescape_html,
+    	decode_crs: decode_crs,
+    	purge: purge,
+    	uri_cache: uri_cache,
+    	cheerio_flags: { decodeEntities: false }
+>>>>>>> 4559137... Merged Rev 13
     };
 
     if(typeof(chap.title) !== 'string')
@@ -181,9 +193,9 @@ for(var i = 0; i < spec.contents.length; i++)
     params.chap.id = '' + i;
     params.chap.dom = cheerio.load('');
 
-    var ops = [];
-    var filter_type = Object.prototype.toString.call(spec.filters);
-
+    let   ops = [];
+	const filter_type = Object.prototype.toString.call(spec.filters);
+	
     if(filter_type === '[object Array]')
     {
 	for(var fi = 0; fi < spec.filters.length; fi++)
@@ -194,8 +206,23 @@ for(var i = 0; i < spec.contents.length; i++)
 
 	if(typeof(chap.filters) !== 'string')
 	{
-	    console.log(ERROR_TAG + 'In "' + chap.title + '": When a collection of filters is specified, each chapter must also specify witch filter chain to use.');
-	    return;
+		
+		if(typeof(chap.filters) !== 'string')
+		{
+			console.log(ERROR_TAG + 'In "' + chap.title + '": When a collection of filters is specified, each chapter must also specify witch filter chain to use.');
+			return;
+		}
+		
+		if(!(chap.filters in spec.filters))
+		{
+			console.log(ERROR_TAG + 'In "' + chap.title + '"; Cannot resolve the filter chain "' + chap.filters + '".');
+			return;
+		}
+		
+		const filters = spec.filters[chap.filters];
+		
+		for(let fi = 0; fi < filters.length; fi++)
+		    ops.push(filter_mgr.get(filters[fi]));
 	}
 
 	if(!(chap.filters in spec.filters))
@@ -221,21 +248,21 @@ for(var i = 0; i < spec.contents.length; i++)
 	sched[chap.src] = [[ops, params]];
 }
 
-for(var src in sched)
+for(let src in sched)
 {
-    if(!sched.hasOwnProperty(src))
-	continue;
-
-    var chapters = sched[src];
-
-    if(chapters.length === 1)
-	Sequence(chapters[0][0], chapters[0][1]);
-    else
-    {
-	Sequence(chapters[0][0], chapters[0][1], function(chapters) { return function()
-	    {
-		for(var ci = 1; ci < chapters.length; ci++)
-		    Sequence(chapters[ci][0], chapters[ci][1]);
-	    }}(chapters));
-    }
+	if(!sched.hasOwnProperty(src))
+		continue;
+	
+	const chapters = sched[src];
+	
+	if(chapters.length === 1)
+		Sequence(chapters[0][0], chapters[0][1]);
+	else
+	{
+		Sequence(chapters[0][0], chapters[0][1], function(chapters) { return function()
+		{
+			for(let ci = 1; ci < chapters.length; ci++)
+				Sequence(chapters[ci][0], chapters[ci][1]);
+		}}(chapters));
+	}
 }
